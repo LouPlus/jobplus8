aeada5f2ff6c69947df8f0e16a7a613a90d836b7
44cat <2018164359@qq.com>
2017-12-28 23:51:55 +0800

    change company route

--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -1,8 +1,21 @@
-from flask import Blueprint,render_template,flash,redirect,url_for
+from flask import Blueprint,render_template,flash,redirect,url_for,current_app,request
 from flask_login import login_required,current_user
 from jobplus.forms import CompanyProfileFrom
+from jobplus.models import CompanyDetail
 
 company = Blueprint('company',__name__,url_prefix='/company')
+
+@company.route('/')
+def index():
+    page = reauest.args.get('page',1,type=int)
+    pagination = CompanyDetail.query.order_by(CompanyDetail.created_at.desc()).paginate(
+            page=page,
+            per_page=current_app.config['INDEX_PER_PAGE'],
+            error_out=False
+            )
+    return render_template('company/index.html',pagination=pagination,active='company')
+
+            
 
 @company.route('/profile',methods=['GET','POST'])
 @login_required
